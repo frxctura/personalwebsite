096e5396a98d0e67eb52f0bd062f9a9ef02c7032
Chax <69131569+Chax1@users.noreply.github.com>
2022-08-18 14:48:19 +0200

    Update playlistbutton.js

--- playlistbutton.js
+++ playlistbutton.js
@@ -1,3 +1,3 @@
 document.getElementById("playlist").onclick = function () {
-    location.href = "https://open.spotify.com/playlist/53pJK0PgyK8kVVlXo2EGiU?si=9f44025a25c546a1";
+    location.href = "https://open.spotify.com/playlist/5fP2Lw1eLbRrGXZcoy1Wik?si=165e64fcb51b4f40";
 };
