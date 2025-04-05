57a6d6573695dd698e490af83f0a98a6feba9c73
Maxine <chaxprivate@gmail.com>
2025-04-05 14:49:48 +0200

    Add files via upload

---
+++ update-avatar.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+fetch("https://api.lanyard.rest/v1/users/468465290531962900")
+ .then((res) => res.json())
+ .then((data) => {
+    if (data.success) {
+      const userId = data.data.discord_user.id;
+      const avatarHash = data.data.discord_user.avatar;
+      const avatarUrl = `https://cdn.discordapp.com/avatars/${userId}/${avatarHash}`;
+
+      // Read the index.html file
+      fs.readFile('index.html', 'utf8', function(err, data) {
+        if (err) {
+          return console.log(err);
+        }
+        // Dynamically find and update the og:image content attribute with the new avatar URL
+        const ogImageRegex = /<meta property="og:image" content="[^"]*">/;
+        const updatedContent = data.replace(ogImageRegex, `<meta property="og:image" content="${avatarUrl}">`);
+
+        // Write the updated content back to index.html
+        fs.writeFile('index.html', updatedContent, 'utf8', function(err) {
+          if (err) return console.log(err);
+        });
+      });
+    } else {
+      console.error("Error fetching avatar data.");
+    }
+ })
+ .catch((error) => {
+    console.error("Error fetching from Lanyard API:", error);
+ });
